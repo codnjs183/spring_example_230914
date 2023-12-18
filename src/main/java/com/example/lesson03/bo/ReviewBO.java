224f4731a4cef485fdf54e67d7f77d2ce78fb8e9
codnjs183 <chaewon1830@gmail.com>
2023-12-18 18:43:53 +0900

    23/12/18 수업내용

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -12,11 +12,11 @@ public class ReviewBO {
 	@Autowired
 	private ReviewMapper reviewMapper;
 	
-	// input: X (나중에 추가한다)
+	// input: id
 	// output: Review 테이블
 	
 	// select일 경우, BO 클래스의 메소드명은 get으로 시작하는 경우가 많다.
-	public Review getReview() {
-		return reviewMapper.selectReview();
+	public Review getReview(int id) {
+		return reviewMapper.selectReview(id);
 	}
 }
